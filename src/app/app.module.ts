4709c4c4d49afdd8988a973f4628f3f7ab7f0b31
José Marcos <mf87660@gmail.com>
2024-02-06 09:45:56 -0300

    :construction: initialized components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FeaturedCardComponent } from './components/featured-card/featured-card.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PostCardComponent } from './components/post-card/post-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FeaturedCardComponent,
+    HomeComponent,
+    PostCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/featured-card/featured-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-featured-card',
+  templateUrl: './featured-card.component.html',
+  styleUrl: './featured-card.component.css'
+})
+export class FeaturedCardComponent {
+
+}
